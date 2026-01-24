79b1d8c4ea037c70fb14d6ce3a6ea4f8ec0a394d
shun <shun.soccer.shun@gmail.com>
2026-01-24 18:28:33 +0900

    feat: 未ログイン時の挙動

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { supabase } from "../../../lib/supabaseClient";
+
+export default function LoginPage() {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      alert("ログインに失敗しました");
+    } else {
+      router.push("/admin");
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <input value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={handleLogin}>ログイン</button>
+      </div>
+    </div>
+  );
+}
